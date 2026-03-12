f7cdbb47b53a5f97084aea7a9edcb40c370cfea0
Amit Arora <aroraai@amazon.com>
2026-03-12 12:07:25 +0000

    fix: update ALB S3 bucket policy to modern service principal approach
    Replace legacy region-specific ELB account IDs with AWS-recommended
    service principal method for ALB access logging.
    
    Changes:
    - Remove elb_service_accounts local variable (legacy approach)
    - Change principal from AWS account ARN to service principal:
      logdelivery.elasticloadbalancing.amazonaws.com
    - Update resource path to include /AWSLogs/{account_id}/* (required)
    - Add condition: s3:x-amz-acl = bucket-owner-full-control
    - Update AclCheck statement to use service principal consistently
    
    Benefits:
    - Works across all regions without region-specific account IDs
    - AWS-recommended approach (Aug 2022+)
    - Simpler and more maintainable
    - No need to update when AWS adds new regions
    
    Reference: https://docs.aws.amazon.com/elasticloadbalancing/latest/application/enable-access-logging.html
    
    Related: #603

--- terraform/aws-ecs/alb-logging.tf
+++ terraform/aws-ecs/alb-logging.tf
@@ -87,36 +87,9 @@ resource "aws_s3_bucket_lifecycle_configuration" "alb_logs" {
 }
 
 
-# Local variable for ELB service account per region
-locals {
-  # https://docs.aws.amazon.com/elasticloadbalancing/latest/application/enable-access-logging.html
-  elb_service_accounts = {
-    us-east-1      = "127311923021"
-    us-east-2      = "033677994240"
-    us-west-1      = "027434742980"
-    us-west-2      = "797873946194"
-    af-south-1     = "098369216593"
-    ca-central-1   = "985666609251"
-    eu-central-1   = "054676820928"
-    eu-west-1      = "156460612806"
-    eu-west-2      = "652711504416"
-    eu-south-1     = "635631232127"
-    eu-west-3      = "009996457667"
-    eu-north-1     = "897822967062"
-    ap-east-1      = "754344448648"
-    ap-northeast-1 = "582318560864"
-    ap-northeast-2 = "600734575887"
-    ap-northeast-3 = "383597477331"
-    ap-southeast-1 = "114774131450"
-    ap-southeast-2 = "783225319266"
-    ap-southeast-3 = "589379963580"
-    ap-south-1     = "718504428378"
-    me-south-1     = "076674570225"
-    sa-east-1      = "507241528517"
-  }
-}
-
 # Bucket policy for ALB logging with TLS enforcement
+# Using modern service principal approach (recommended by AWS)
+# https://docs.aws.amazon.com/elasticloadbalancing/latest/application/enable-access-logging.html
 resource "aws_s3_bucket_policy" "alb_logs" {
   bucket = aws_s3_bucket.alb_logs.id
 
@@ -145,16 +118,21 @@ resource "aws_s3_bucket_policy" "alb_logs" {
         Sid    = "AWSLogDeliveryWrite"
         Effect = "Allow"
         Principal = {
-          AWS = "arn:aws:iam::${local.elb_service_accounts[var.aws_region]}:root"
+          Service = "logdelivery.elasticloadbalancing.amazonaws.com"
         }
         Action   = "s3:PutObject"
-        Resource = "${aws_s3_bucket.alb_logs.arn}/*"
+        Resource = "${aws_s3_bucket.alb_logs.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
       },
       {
         Sid    = "AWSLogDeliveryAclCheck"
         Effect = "Allow"
         Principal = {
-          Service = "elasticloadbalancing.amazonaws.com"
+          Service = "logdelivery.elasticloadbalancing.amazonaws.com"
         }
         Action   = "s3:GetBucketAcl"
         Resource = aws_s3_bucket.alb_logs.arn
